5931d57baaf7d8a4bd31b3d6bc8e5c357f8b7c1d
0x74h51N <mr.tahsin@windowslive.com>
2024-07-03 15:03:41 +0300

    fix

--- src/app/[id]/_components/Transactions/Method.tsx
+++ src/app/[id]/_components/Transactions/Method.tsx
@@ -1,20 +1,27 @@
+import React from "react";
+import { isAddress } from "viem";
+
 interface MethodProps {
-  from: `0x${string}` | null;
-  to: `0x${string}` | null;
+  from: string | null;
+  to: string | null;
 }
 
 const PANCAKE_SWAP_ADDRESSES = ["0x67D8fdFba16Cc3300d34148217f25c50BDF31083"];
 
 const Method: React.FC<MethodProps> = ({ from, to }) => {
-  const getMethodName = (from: `0x${string}` | null, to: `0x${string}` | null): string => {
+  const getMethodName = (from: string | null, to: string | null): string => {
     if (from === "0x0000000000000000000000000000000000000000" || !from) {
       return "Mint";
     } else if (to === "0x0000000000000000000000000000000000000000" || !to) {
       return "Burn";
-    } else if (PANCAKE_SWAP_ADDRESSES.includes(from) || PANCAKE_SWAP_ADDRESSES.includes(to)) {
-      return "PnckSwap";
+    } else if (isAddress(from) && isAddress(to)) {
+      if (PANCAKE_SWAP_ADDRESSES.includes(from) || PANCAKE_SWAP_ADDRESSES.includes(to)) {
+        return "PnckSwap";
+      } else {
+        return "Transfer";
+      }
     } else {
-      return "Transfer";
+      return "Unknown";
     }
   };
 

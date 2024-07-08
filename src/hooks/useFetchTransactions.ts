f7275557b3f0f8366ce54380019f599e82e36a92
0x74h51N <mr.tahsin@windowslive.com>
2024-07-08 17:20:58 +0300

    fetch timing

--- src/hooks/useFetchTransactions.ts
+++ src/hooks/useFetchTransactions.ts
@@ -49,8 +49,9 @@ const useFetchTransactions = (all: boolean, testnet: boolean, address: Address):
   useEffect(() => {
     if (sessionStart) {
       if (all && !globalTransactions) {
-        setTimeout(() => fetchTransactions(), 500);
+        fetchTransactions();
       } else if (!all) {
+        setTimeout(() => fetchTransactions(), 500);
         const interval = setInterval(() => {
           fetchTransactions();
         }, 30000);

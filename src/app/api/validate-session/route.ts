9ee1e05f73547d7ebb9fb69817613227e2d29338
0x74h51N <mr.tahsin@windowslive.com>
2024-07-26 15:10:01 +0300

    fix

--- src/app/api/validate-session/route.ts
+++ src/app/api/validate-session/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getIronSession } from "iron-session";
 import { SessionData, sessionOptions } from "~~/lib/sessionOptions";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
   if (session.isLoggedIn && session.walletAddress) {

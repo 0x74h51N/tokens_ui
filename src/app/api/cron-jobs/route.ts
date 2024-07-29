9e736a4dd5f9d14589de6dbb5e5ae66b447026ee
0x74h51N <mr.tahsin@windowslive.com>
2024-07-29 10:52:17 +0300

    cron

--- src/app/api/cron-jobs/route.ts
+++ src/app/api/cron-jobs/route.ts
@@ -3,8 +3,10 @@ import { runCronJobs } from "~~/services/cron/cronJobs";
 
 export async function GET() {
   try {
-    await runCronJobs();
-    return NextResponse.json({ message: "Cron jobs completed successfully" }, { status: 200 });
+    const response = await runCronJobs();
+    if (response) {
+      return NextResponse.json({ message: "Cron jobs completed successfully" }, { status: 200 });
+    }
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });

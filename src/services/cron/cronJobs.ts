350983aa757a5a456ba5bf304236cd754e881609
0x74h51N <mr.tahsin@windowslive.com>
2024-07-29 11:30:05 +0300

    debug

--- src/services/cron/cronJobs.ts
+++ src/services/cron/cronJobs.ts
@@ -9,9 +9,11 @@ const cronSecret = process.env.CRON_SECRET;
 const testnetAddresses = scaffoldConfig.testnetContractAddressList || [];
 const mainnetAddresses = scaffoldConfig.contractAddressList || [];
 const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://tokens-ui.vercel.app";
+console.log(`Base URL: ${baseUrl}`);
 
 async function fetchTransactions(contractAddress: string, testnet: boolean) {
   const url = `${baseUrl}/api/fetch-transactions?contractaddress=${contractAddress}&testnet=${testnet}&allTx=true&cleanCache=true`;
+  console.log(`Fetching transactions from URL: ${url}`);
   const response: Response = await fetch(url, {
     method: "GET",
     headers: {
@@ -20,17 +22,26 @@ async function fetchTransactions(contractAddress: string, testnet: boolean) {
   });
 
   if (!response.ok) {
+    console.error(`Failed to fetch transactions: ${response.status} ${response.statusText}`);
     throw new Error(`Failed to fetch transactions for ${contractAddress}`);
   }
 
   const data = (await response.json()) as FetchTransactionsResponse;
+  console.log(`Transactions fetched for ${contractAddress}: ${JSON.stringify(data)}`);
   return data.message;
+}
+
+async function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export async function runCronJobs() {
   let resultMessage = "Cron jobs completed with the following results:\n";
 
+  await delay(500);
+
   for (const address of testnetAddresses) {
+    console.log(`Fetching transactions for testnet address: ${address}`);
     try {
       const message = await fetchTransactions(address, true);
       console.log(`Successfully fetched transactions for testnet address: ${address}`);
@@ -41,9 +52,11 @@ export async function runCronJobs() {
         resultMessage += `Testnet address ${address}: ${error.message}\n`;
       }
     }
+    await delay(500);
   }
 
   for (const address of mainnetAddresses) {
+    console.log(`Fetching transactions for mainnet address: ${address}`);
     try {
       const message = await fetchTransactions(address, false);
       console.log(`Successfully fetched transactions for mainnet address: ${address}`);
@@ -54,6 +67,7 @@ export async function runCronJobs() {
         resultMessage += `Mainnet address ${address}: ${error.message}\n`;
       }
     }
+    await delay(500);
   }
 
   console.log(resultMessage);

9e37a8e8f3f008fdfa332ce0de6fe4615b97bf62
0x74h51N <mr.tahsin@windowslive.com>
2024-07-30 16:28:23 +0300

    rest

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,21 +8,12 @@ const publicPaths = ["/api/login", "/api/validate-session", "/api/logout", "/log
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/api/")) {
-    const allowedOrigins = [process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"];
-    const origin = req.headers.get("origin");
-
-    if (!origin || !allowedOrigins.includes(origin)) {
-      return new NextResponse("CORS policy error", { status: 403 });
-    }
-  }
+  const cronSecret = process.env.CRON_SECRET;
 
   if (publicPaths.includes(pathname)) {
     return res;
   }
 
-  const cronSecret = process.env.CRON_SECRET;
   const authHeader = req.headers.get("Authorization");
   if (authHeader === `Bearer ${cronSecret}`) {
     console.log("Authorization successful");
